9eca358fbeabc173cb0ddcabb8a040eaca0a23fe
AdibHoque <adibhoque04@gmail.com>
2024-06-01 16:24:00 +0600

    Initial Vite React Project with TailwindCSS, Material Tailwind UI & React Router

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import {Button} from "@material-tailwind/react";
+
+function App() {
+  return (
+    <>
+      <Button className="mx-10 my-2">Button</Button>
+    </>
+  );
+}
+
+export default App;
